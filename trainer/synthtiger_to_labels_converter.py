0473ffa32789fbb685a821874ed7f31bbbcf056f
Mukhtar Bimurat <bimurat.mukhtar@gmail.com>
2023-09-12 11:31:37 +0600

    added new model code

--- trainer/synthtiger_to_labels_converter.py
+++ trainer/synthtiger_to_labels_converter.py
@@ -29,8 +29,8 @@ def convert(root):
 
 
 def main():
-    convert(f'{gt_dir}/train_v2')
-    convert(f'{gt_dir}/test_v2')
+    convert(f'{gt_dir}/train_v3')
+    convert(f'{gt_dir}/test_v3')
 
 
 if __name__ == '__main__':
